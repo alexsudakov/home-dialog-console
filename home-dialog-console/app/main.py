682b428ad63c993055e1cc3638e3bb7c28f5d4af
root <root@a0d7b954-ssh.local.hass.io>
2026-05-29 16:44:35 +0300

    Make HDC navigation ingress safe

--- home-dialog-console/app/main.py
+++ home-dialog-console/app/main.py
@@ -18,7 +18,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-APP_VERSION = "0.1.32"
+APP_VERSION = "0.1.33"
 CONFIG_PATH = Path("/data/options.json")
 DEFAULT_DIALOG_SERVICE_URL = "http://127.0.0.1:8090"
 DEFAULT_RETRIEVAL_SERVICE_URL = "http://192.168.1.138:8085"
